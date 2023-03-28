d3404008dd7f4d61399b1419538d1183d51fc2c8
Lakshya <lakshyabamne181@gmail.com>
2023-03-28 16:53:39 +0530

    [EDA] Added a details page to give general information about the project in the streamlit app.

---
+++ bin/pages/Project_Details.py
@@ -0,0 +1,46 @@
+# LAST PAGE to show the details of the project like
+# => Team members
+# => Project workflow
+# => Data Pipelines etc.
+
+#############################################################################################
+import streamlit as st
+#############################################################################################
+
+st.write("""
+    # Project Details
+
+    ---
+
+    ### Team Members
+
+    * Lakshya Bamne (20MA20029) :wink:
+    * Sarthak Raj (pogu) (20MA20050) :kissing_heart:
+    * Swapnil Shekhar (20MA20059) :innocent:
+    * Md Israil (20MA20032) :sunglasses:
+    * Vibhav Dubey (20MA20063) :laughing: 
+    * Vivek Rajpurohit (20MA20064) :zany_face:
+    * Priyanshu Kumar (20MA20047) :zipper_mouth_face:
+    * Prateek Nagar (20MA20045) :mask:
+    * Harsh Wasnik (20MA20023) :clown_face:
+    * Shekhar Kumar (20MA20052) :smiley_cat:
+    * Pawan Nannaware (20MA20036) :relieved:
+
+    ---
+
+    #### General info
+
+    * This is the term project we created for the course ***Big Data Analysis (MA60306)***
+    we took in Semester-6 at IIT Kharagpur.
+
+    ---
+
+    #### Project Links
+
+    * [GitHub](https://github.com/lakshyaBamne/BIG_DATA_TermProject_SEM6_ThePredictivePros)
+    :point_left:
+
+    ---
+""")
+
+
