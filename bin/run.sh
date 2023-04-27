66c6f852d81dffe4901dad19d2eaceeea5be651e
Lakshya <lakshyabamne181@gmail.com>
2023-04-27 11:22:59 +0530

    COMMITTED BEFORE DELETING LOCAL REPO FOR CLEANING

--- bin/run.sh
+++
@@ -1,1 +0,0 @@
-python scrape.py
--- bin/scraper/Countries.py
+++ bin/scraper/Countries.py
@@ -2,17 +2,17 @@
 # we are including the top 12 ODI ranked teams in the world at the time of making this project
 country_list = [
     "IND",  # India
-    "AUS",  # Australia
-    "NZL",  # New Zealand
-    "ENG",  # England
-    "PAK",  # Pakistan
-    "SAF",  # South Africa
-    "BAN",  # Bangladesh
-    "SRL",  # Sri Lanka
+    # "AUS",  # Australia
+    # "NZL",  # New Zealand
+    # "ENG",  # England
+    # "PAK",  # Pakistan
+    # "SAF",  # South Africa
+    # "BAN",  # Bangladesh
+    # "SRL",  # Sri Lanka
     # "AFG",  # Afghanistan
-    "WIN",  # West Indies
-    "IRE",  # Ireland
-    "SCO"   # Scotland
+    # "WIN",  # West Indies
+    # "IRE",  # Ireland
+    # "SCO"   # Scotland
 ]
 
 espn_country_list = {
